c5d4d47f057f19f7360f69108dfca3d37752f0e8
User <karterr.r@yandex.ru>
2024-04-09 15:20:45 +0500

    начало2

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return render_template('base.html', title='Вы тут найдете всё')
+    photo = [["/static/img/one.PNG", "/static/img/two.PNG", "/static/img/one.PNG", "/static/img/two.PNG"],
+             ["/static/img/one.PNG", "/static/img/two.PNG", "/static/img/one.PNG", "/static/img/two.PNG",
+             "/static/img/one.PNG"]]
+    return render_template('index.html', title='Вы тут найдете всё', photos=photo,
+                           css1=url_for('static', filename='css/style.css'))
 
 
 @app.route('/register', methods=['GET', 'POST'])
